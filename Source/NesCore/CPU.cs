177e46ae36592739f1e44a4df33de9f4532d4b40
nvanderpyle <nick.vp@gmail.com>
2013-12-01 07:35:46 -0600

    Adding support for some OpCodes
    Stepping through a Mario rom and implementing opcodes as encountered.
    Using references:
    http://www.nintendoage.com/forum/messageview.cfm?catid=22&threadid=4440
    http://6502.org/tutorials/6502opcodes.html

--- Source/NesCore/CPU.cs
+++ Source/NesCore/CPU.cs
@@ -4,8 +4,13 @@ namespace NesCore
 {
 	public class CPU
 	{
-		private int _executionPointer = 0xFFFC;
+		private const int InitialExecutionAddress = 0xFFFC;
+		private int _programCounter;
+		private byte _accumulator;
 		private readonly Memory _memory;
+		private bool _cpuFlagSEI = false;
+		private bool _cpuFlagCLD = false;
+		private bool _isResetting = true;
 
 		public CPU (Memory memory)
 		{
@@ -14,9 +19,36 @@ namespace NesCore
 
 		public void OneCpuCycle ()
 		{
-			var address = _memory.ReadUInt16 (_executionPointer);
-			Byte opCode = _memory.ReadByte (address);
-			Console.WriteLine (opCode);
+			if (_isResetting) {
+				_isResetting = false;
+				_programCounter = _memory.ReadUInt16 (InitialExecutionAddress);
+			}
+
+			Byte opCode = _memory.ReadByte (_programCounter);
+			_programCounter++;
+
+			// opcodes and  http://6502.org/tutorials/6502opcodes.html#SEI
+			switch (opCode) {
+			case 0x78:	//SEI (SEt Interrupt)
+				_cpuFlagSEI = true;
+				break;
+			case 0xD8:	//CLD (CLear Decimal)
+				_cpuFlagCLD = true;
+				break;
+			case 0xA9:	//LDA (LoaD Accumulator)
+				_accumulator = _memory.ReadByte (_programCounter);
+				_programCounter++;
+				break;
+			case 0x8D:	//STA (STore Accumulator)
+				var addressToStoreAccumulator = _memory.ReadUInt16 (_programCounter);
+				_programCounter += 2;
+				_memory.WriteByteToAddress (_accumulator, addressToStoreAccumulator);
+				break;
+			case 0xA2:	//LDX (LoaD X register)
+				break;
+			default:
+				throw new NotImplementedException (string.Format ("OpCode not implemented: {0:X2}", opCode));
+			}
 		}
 	}
 }
--- Source/NesCore/Memory.cs
+++ Source/NesCore/Memory.cs
@@ -6,21 +6,10 @@ namespace NesCore
 	{
 		private const int ROMOFFSET = 0x8000;
 		private readonly byte[] _bytes;
-		private int _position;
 
 		public Memory (int sizeInBytes)
 		{
-			_position = 0;
 			_bytes = new byte[sizeInBytes];
-		}
-
-		public int Position {
-			get {
-				return _position;
-			}
-			set {
-				_position = value;
-			}
 		}
 
 		public void LoadRom (NesRom rom)
@@ -30,8 +19,6 @@ namespace NesCore
 
 		public UInt16 ReadUInt16 (int fromAddress)
 		{
-			_position += 2;
-
 			var lowByte = (int)_bytes [fromAddress];
 			var highByte = (int)_bytes [fromAddress + 1];
 			var aNumber = (UInt16)((highByte << 8) | lowByte);
@@ -42,6 +29,11 @@ namespace NesCore
 		public byte ReadByte (int fromAddress)
 		{
 			return _bytes [fromAddress];
+		}
+
+		public void WriteByteToAddress (byte byteToWrite, int addressToStoreAccumulator)
+		{
+			_bytes [addressToStoreAccumulator] = byteToWrite;
 		}
 	}
 }
